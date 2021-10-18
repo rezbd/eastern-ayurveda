f50b4d8c51976b99153b792273f0f1df5b2f1ed1
rezwan <rezwan6@gmail.com>
2021-10-18 19:35:37 +0600

    implemented responsiveness using media query

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import "./Service.css";
 
 const Service = ({ service }) => {
     const { img, title, description } = service;
     return (
-        <div>
-            <img src={img} alt="" />
+        <div className="medicine">
+            <img className="medicine-img" src={img} alt="" />
             <h3>{title}</h3>
             <h5>{description}</h5>
+            <button>Details</button>
         </div>
     );
 };
