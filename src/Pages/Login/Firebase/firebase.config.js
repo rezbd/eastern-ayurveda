edd2afc0a43217e7890d3af49c01fb44ad348159
rezwan <rezwan6@gmail.com>
2021-10-19 08:24:46 +0600

    responsive header and installed firebase. six-one-four

---
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyByv3ELfLSof6nK0WvwIbLyVx6MAvcmkzo",
+    authDomain: "eastern-ayurveda.firebaseapp.com",
+    projectId: "eastern-ayurveda",
+    storageBucket: "eastern-ayurveda.appspot.com",
+    messagingSenderId: "847471640868",
+    appId: "1:847471640868:web:1cda427d3840f286f6f3b1"
+};
+
+export default firebaseConfig;
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <>
-            <Navbar bg="dark" variant="dark" sticky="top">
+            <Navbar bg="dark" variant="dark" sticky="top" collapseOnSelect expand="lg">
                 <Container>
                     <Navbar.Brand href="#home">Eastern Ayurveda</Navbar.Brand>
                     <Navbar.Toggle />
                     <Navbar.Collapse className="justify-content-end">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#services">Medicines</Nav.Link>
-                        <Nav.Link href="#herbs">Herbs</Nav.Link>
+                        <Nav.Link as={Link} to="/home#home">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/home#services">Medicines</Nav.Link>
+                        <Nav.Link as={Link} to="/home#herbs">Herbs</Nav.Link>
+                        <Nav.Link as={Link} to="/login">Login</Nav.Link>
                         <Navbar.Text>
                             Signed in as: <a href="#login">Mark Otto</a>
                         </Navbar.Text>
