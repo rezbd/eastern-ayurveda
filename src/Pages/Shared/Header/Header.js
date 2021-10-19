9618c8138092c5b12589ea9184203b4506ae5e64
rezwan <rezwan6@gmail.com>
2021-10-19 18:29:10 +0600

    hosted on firebase. six-one-nine

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import useAuth from '../../../hooks/useAuth';
+import { HashLink } from 'react-router-hash-link';
 
 const Header = () => {
     const { user, logOut } = useAuth();
@@ -12,9 +13,9 @@ const Header = () => {
                     <Navbar.Brand href="#home">Eastern Ayurveda</Navbar.Brand>
                     <Navbar.Toggle />
                     <Navbar.Collapse className="justify-content-end">
-                        <Nav.Link as={Link} to="/home#home">Home</Nav.Link>
-                        <Nav.Link as={Link} to="/home#services">Medicines</Nav.Link>
-                        <Nav.Link as={Link} to="/home#herbs">Herbs</Nav.Link>
+                        <Nav.Link as={HashLink} to="/home#home">Home</Nav.Link>
+                        <Nav.Link as={HashLink} to="/home#services">Medicines</Nav.Link>
+                        <Nav.Link as={HashLink} to="/home#herbs">Herbs</Nav.Link>
                         {user?.email ?
                             <Button onClick={logOut} variant="dark">Logout</Button> :
                             <Nav.Link as={Link} to="/login">Login</Nav.Link>
