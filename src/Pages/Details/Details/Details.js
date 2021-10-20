ab83725200e52dc0b5847edc863289d995ab8f27
rezwan <rezwan6@gmail.com>
2021-10-20 12:12:05 +0600

    added details

--- src/Pages/Details/Details/Details.js
+++ src/Pages/Details/Details/Details.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import { useParams } from 'react-router';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const Details = () => {
     const { serviceId } = useParams();
+    const [post, setPost] = useState([]);
+    const [med, setMed] = useState([]);
+
+    useEffect(() => {
+        fetch("/medData.json")
+            .then((res) => res.json())
+            .then(data => setPost(data))
+    }, []);
+    useEffect(() => {
+        const foundPost = post.find(item => item.id === serviceId)
+        setMed(foundPost);
+    }, [post]);
+
     return (
-        <div>
-            <h2>Details of the medicine {serviceId}</h2>
+        <div className="w-50 mx-auto">
+            <h3 className="mt-4">Medicine: {med?.title}</h3>
+            <img src={med?.img} alt="" />
+            <p className="mt-4"><strong>Description</strong>: {med?.description}</p>
+            <p className="mt-4 pb-4"><strong>Indications</strong>: {med?.indications}</p>
         </div>
     );
 };
