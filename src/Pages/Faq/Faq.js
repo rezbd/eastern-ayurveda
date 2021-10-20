c4fc38b84c6caf06e9bb7305d69951d5b09cf4e7
rezwan <rezwan6@gmail.com>
2021-10-20 23:32:50 +0600

    faq text update

--- src/Pages/Faq/Faq.js
+++ src/Pages/Faq/Faq.js
@@ -10,109 +10,55 @@ const Faq = () => {
                 <Accordion.Item eventKey="0" className="faq-body">
                     <Accordion.Header>What is yoga?</Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.
+                        Yoga is an ancient practice that involves physical poses, concentration, and deep breathing. A regular yoga practice can promoteTrusted Source endurance
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="1">
                     <Accordion.Header>What is Ayurveda?</Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.
+                        Ayurveda is an alternative medicine system with historical roots in the Indian subcontinent. The theory and practice of Ayurveda is pseudoscientific. The Indian Medical Association describes Ayurvedic practitioners who claim to practice medicine as quacks.
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="2">
                     <Accordion.Header>Is Ayurveda Effective?</Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.
+                        It is common to hear a general statement that Ayurveda lacks scientific evidence. By scientific evidence, it is commonly referred to results of human clinical trials undertaken adoring those applied to pharmaceuticals, involving randomized controlled trials (RCT), either a placebo or active controlled.
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="3">
-                    <Accordion.Header>What are the benefits?</Accordion.Header>
+                    <Accordion.Header>What are the benefits of Ayurbeda?</Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.
+                        Ayurveda blends our modern lifestyle and health-oriented habits with the ancient wisdom of using natural substances, medicines and herbs to help us lead a healthy, happy, stress-free and disease-free life. ... The cardinal aim of Ayurveda is to restore the individual balance between mind, body and spirit.
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="4">
-                    <Accordion.Header>Is Yoga and Ayurveda is same?</Accordion.Header>
+                    <Accordion.Header>Is Yoga and Ayurveda same?</Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.
+                        Yoga and Ayurveda two distinct sciences yet share the same Vedic roots. ... While yoga deals with the harmonizing of mind, body and soul while Ayurveda takes care of the person's both physical and mental wellbeing through the means of diet and lifestyle changes.
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="5">
-                    <Accordion.Header>What are the side effects?</Accordion.Header>
+                    <Accordion.Header>What are the side effects of Ayurveda?</Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.
+                        Among some of the side effects you should watch out for: Triphala: diarrhea and abdominal discomfort, especially in high doses. Guggul: stomach upset, headaches, nausea, vomiting, loose stools, diarrhea, belching, and hiccups. Boswellia: stomach pain, nausea, diarrhea, and an allergic rash (when applied to the skin)
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="6">
                     <Accordion.Header>Is Ayurveda a lifestyle?</Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.
+                        Ayurveda is an ancient science of life since time immemorial. Ayurveda, apart from providing various therapeutic measures for diseases, emphasizes on maintenance, promotion of health and prevention of diseases through diet and lifestyle regimens.
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="7">
-                    <Accordion.Header>Is it safe to take the medicines?</Accordion.Header>
+                    <Accordion.Header>Is it safe to take the ayurvedic medicines?</Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.
+                        Ayurveda is one of the most ancient systems of treatment in India. These medicines are effective and time tested. They help one regain energy and enhance well-being. Maharishi Ayurvedic medicine, for one, is safe and backed up with scientific evidence.
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="8">
-                    <Accordion.Header>How can I learn more?</Accordion.Header>
+                    <Accordion.Header>How can I learn more about Ayurveda?</Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.
+                        Ideally, getting to know yourself should be the first step for learning Ayurveda. Observing your emotions and paying attention to small details about your body and mind.
                     </Accordion.Body>
                 </Accordion.Item>
             </Accordion>
